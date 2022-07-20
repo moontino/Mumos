3212d2f384d2aa46fa7025053061351fedfafa7e
Ventsislava V Genova <73789010+moontino@users.noreply.github.com>
2022-07-20 17:21:47 +0300

    Add nav/header

--- Mumos/ClientApp/src/components/NavMenu.js
+++ Mumos/ClientApp/src/components/NavMenu.js
@@ -25,31 +25,54 @@ export class NavMenu extends Component {
     render() {
         return (
             <header>
-                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-                    <Container>
-                        <NavbarBrand tag={Link} to="/">Mumos</NavbarBrand>
-                        <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-                        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-                            <ul className="navbar-nav flex-grow">
-                                <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/data">Data</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                                </NavItem>
-                                <LoginMenu>
-                                </LoginMenu>
-                            </ul>
-                        </Collapse>
-                    </Container>
-                </Navbar>
+                <div className="navigation w-100">
+                    <div className="container">
+                        <nav className="navbar navbar-expand-lg navbar-light p-0">
+                            <a className="navbar-brand" href="index.html">
+                                <img src="images/logo.png" alt="logo" />
+                            </a>
+                            <button
+                                className="navbar-toggler rounded-0"
+                                type="button"
+                                data-toggle="collapse"
+                                data-target="#navigation"
+                                aria-controls="navigation"
+                                aria-expanded="false"
+                                aria-label="Toggle navigation"
+                            >
+                                <span className="navbar-toggler-icon" />
+                            </button>
+                            <div className="collapse navbar-collapse" id="navigation">
+
+                                <ul className="navbar-nav ml-auto text-center">
+                                    <NavItem className="nav-item active">
+                                        <NavLink tag={Link} className="nav-link" to="/">
+                                            Home
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem className="nav-item @@about">
+                                        <NavLink tag={Link} className="nav-link" to="/data">
+                                            Data
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem className="nav-item @@courses">
+                                        <NavLink tag={Link} className="nav-link" to="/counter">
+                                            Counter
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem className="nav-item @@events">
+                                        <NavLink tag={Link} className="nav-link" to="/fetchdata">
+                                            FetchData
+                                        </NavLink>
+                                    </NavItem>
+                                    <LoginMenu>
+                                    </LoginMenu> 
+                                    </ul>
+                            </div>
+                        </nav>
+                    </div>
+                    </div>
             </header>
-        );
+                );
     }
 }
